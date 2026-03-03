b17fd68d5516687f9214ec7a720455ff7ce3122e
Pedro Henrique <ph29@outlook.com.br>
2026-03-03 16:58:38 -0300

    refactor: code sanitization

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from views.render_preview_view import RenderPreview
 from views.form_view import Form
 from services.search_file_service import SearchFile
+from services.upload_service import UploadFile
 from services.decompressed_service import DecompressedFile
 
 
@@ -10,17 +11,20 @@ class App:
     def __init__(self):
         self.app= tk.Tk()
         self.app.geometry('1000x550+280+100')
+        self.app.resizable(False, False) # comente esta variavél de instância para permitir tela cheia
         self.app.title('Sistema de Upload de Arquivos - SisUpa')
 
+        # Serives
         search_service= SearchFile()
         decompress_service= DecompressedFile()
-        
-        
+        upload_service= UploadFile()
+
+        # Views
         self.form= Form()
         self.preview= RenderPreview()
         
-        self.form.form(self.app, search_service, self.preview.action_update_list)
-
+        
+        self.form.form(self.app, search_service, upload_service, self.preview.action_update_list)
         self.preview.frame_preview(self.app, decompress_service)
 
         self.app.mainloop()
